2842c9afa7fa29dd65052bab6c27342b7f6cf592
sunsingbeamm <202201616@hufs.ac.kr>
2026-01-17 01:57:55 +0900

    [FIX]capmpus_optional

--- src/main/java/com/Globoo/auth/dto/VerifyCodeReq.java
+++ src/main/java/com/Globoo/auth/dto/VerifyCodeReq.java
@@ -4,11 +4,10 @@ package com.Globoo.auth.dto;
 import com.Globoo.user.domain.Campus;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record VerifyCodeReq(
         @Email @NotBlank String email,
         @NotBlank @Size(min = 6, max = 6) String code,
-        @NotNull Campus campus              // optional (SEOUL/GLOBAL)
+        Campus campus              // optional (SEOUL/GLOBAL)
 ) {}
